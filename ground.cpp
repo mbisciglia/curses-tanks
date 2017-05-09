979fe78ab15dd351466b854831ea4437c479db4b
lprotasova <lprotasova@carthage.edu>
2017-05-08 23:29:21 -0500

    changed ground initialization
    made chance of going up or down increase. got rid of magic numbers and
    made a const variable instead, and added comment for one of the existing
    vars

--- ground.cpp
+++ ground.cpp
@@ -3,23 +3,30 @@
 
 int lines = 40;
 int cols = 120;
-int base_height_divisor = 8;
+/*	this is a constant that determines how far down the screen the ground starts
+	having it be 8 means we start 1/8th of the screen's height up
+	having it be 2 means we start 1/2 of the screen's height up
+	*/
+int base_height_divisor = 3;
 int max_height_divisor = 2;
 
 void Ground::InitializeGround()
 {
 	int current_height = lines - lines / base_height_divisor;
 	int maximum_height = lines / max_height_divisor;
+	//this is a const that can be 0 to 5000. the greater the number the more the ground will vary
+	const int VOLATILITY = 2000;
 
 	for (int i = 1; i < cols - 1; i++) {
 		int h = current_height;
 		int r = rand() % 10000;
-		if (r < 800) {
+
+		if (r < VOLATILITY) {
 			current_height++;
 			if (current_height >= lines - 2)
 				current_height = lines - 2;
 		}
-		else if (r < 1600) {
+		else if (r < VOLATILITY * 2) {
 			current_height--;
 			if (current_height < 1)
 				current_height = 1;
